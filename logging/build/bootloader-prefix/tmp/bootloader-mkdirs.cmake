78d41c6d025c4efc96c37e8a089706af21e340a2
1337encrypted <smallaszero@gmail.com>
2024-01-28 12:19:05 +0530

    converted logging into an idf project

---
+++ logging/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/shady/esp/esp-idf/components/bootloader/subproject"
+  "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader"
+  "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix"
+  "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix/tmp"
+  "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix/src/bootloader-stamp"
+  "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix/src"
+  "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/Users/shady/Desktop/esp32-fundamentals/logging/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ logging/build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/Users/shady/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
